fcafbf18eaeacfaf2c4f661dc20f8d349224f70a
Guysit Koonrungruang <guy2jzvvt@gmail.com>
2012-10-06 15:12:07 +0700

    complete UI
    connect it to database

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Login log = new Login();
+        Loging log = new Loging();
         log.run();
         Vote vote = new Vote();
         vote.run();
