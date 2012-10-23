80ca5669cefe225700d0506e98539e2b666ba8b7
peepo3663 <peepo157@gmail.com>
2012-10-24 02:05:06 +0700

    Add test package and test cases

---
+++ src/test/TestProject.java
@@ -0,0 +1,27 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class TestProject extends TestCase {
+
+	public void testProject() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetProjectName() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetTeamName() {
+		fail("Not yet implemented");
+	}
+
+}
