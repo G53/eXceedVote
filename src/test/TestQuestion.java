0fadbc170ecdb751f305a5600ae20078a7d83ced
metas4000 <metas4000@hotmail.com>
2012-10-24 07:57:51 +0700

    implemented testQuestion
    implemented testQuestion all method

--- src/test/TestQuestion.java
+++ src/test/TestQuestion.java
@@ -3,28 +3,56 @@ package test;
 import junit.framework.TestCase;
 import domain.Question;
 
+/**
+ * This class is about testing a Question Object
+ * @author Metas Pongmetha
+ * @version 2012/10/24
+ */
 public class TestQuestion extends TestCase {
 	// relate attribute	to test
 	private Question q  = new Question(1,"What your Name?");
-	
+	private String newQuestion = "What is your name?";
+
+	/**
+	 *  Test object have to be created
+	 */
 	public void testQuestion() {
-		fail("Not yet implemented");
+		Question tempq = q;
+		assertSame(tempq, q);
+		Question temp = null;
+		assertNotSame(q, temp);
+		assertNotNull(q);
 	}
 
+	/**
+	 * Test setQuestion method of Question Object.
+	 */
 	public void testSetQuestion() {
-		fail("Not yet implemented");
+		Question tempqq = q;
+		tempqq.setQuestion(newQuestion);
+		assertFalse(q.getQuestion(), tempqq.getQuestion());
+		assertEquals("What is your name ?", tempqq.getQuestion());
 	}
 
+	/**
+	 *  Test GetQuestionID method of Question Object.
+	 */
 	public void testGetQuestionID() {
-		fail("Not yet implemented");
+		assertEquals(1, q.getQuestionID());
 	}
 
+	/**
+	 * Test GetQuestion method of Question Object.
+	 */
 	public void testGetQuestion() {
-		fail("Not yet implemented");
+		assertEquals("What your Name?", q.getQuestion());
 	}
 
+	/**
+	 *  Test toString method of Question Object.
+	 */
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals(q.getQuestion());
 	}
 
 }
