2fc578f3a7088cda5c77ca3b2c1d29fc74589f21
metas4000 <metas4000@hotmail.com>
2012-11-18 04:30:15 +0700

    Modify main for LoadingUI and fix some bug

--- src/g53/exceedvote/domain/Main.java
+++ src/g53/exceedvote/domain/Main.java
@@ -7,8 +7,7 @@ import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 
 import g53.exceedvote.controller.Controller;
-import g53.exceedvote.persistence.VoterDao;
-import g53.exceedvote.persistence.jdbc.VoterDaoJdbc;
+import g53.exceedvote.ui.InterfaceUI;
 import g53.exceedvote.ui.LanguageUI;
 import g53.exceedvote.ui.LoadingUI;
 import g53.exceedvote.ui.LoginUI;
@@ -20,57 +19,65 @@ import g53.exceedvote.ui.VoteUI;
  */
 
 /**
- * @author	Metas Pongmetha 5310546529
- * @Version 2012.November.15
+ * @author Metas Pongmetha 5310546529
+ * @Version 2012.November.18
  */
 public class Main {
 	/**
 	 * @param args
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 * @throws InterruptedException
 	 * @throws SQLException
 	 */
-	public static void main(String[] args) throws UnsupportedEncodingException {
-		ResourceBundle language = ResourceBundle.getBundle("Language");
-		LanguageUI languageUI = new LanguageUI(language);
+	public static void main(String[] args) {
+		LanguageUI languageUI = new LanguageUI();
 		languageUI.run();
 		while (!languageUI.isChoose) {
 			try {
-				Thread.sleep(200);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		//set language
+		ResourceBundle language = languageUI.getLanguage();
 		Controller control = new Controller();
+		//counter for check timeout
 		int i = 0;
-		LoadingUI loadUI = new LoadingUI();
-		while(!control.connect()){
+		InterfaceUI loadUI = new LoadingUI(language);
+		do {
 			try {
-				Thread.sleep(500);
+				Thread.sleep(100);
 				i++;
-				if (i>20){
-					JOptionPane.showMessageDialog(null, control.getCurMessage());
+				//timeout after wait 5 sec
+				if (i > 100) {
+					JOptionPane
+							.showMessageDialog(null, control.getCurMessage());
+					loadUI.close();
 					return;
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		LoginUI loginUI = new LoginUI(control,language);
+		} while (!control.connect());
+		loadUI.close();
+		//run loginUI
+		LoginUI loginUI = new LoginUI(control, language);
 		loginUI.run();
 		while (!loginUI.getSatus()) {
 			try {
-				Thread.sleep(500);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		VoteUI voteUI = new VoteUI(control);
+		//login success
+		//create voteUI for vote
+		VoteUI voteUI = new VoteUI(control, language);
 		voteUI.run();
-		
 
 	}
 
