41125d9c1c7b3855b3a889bee51e70666692c23c
peepo3663 <peepo157@gmail.com>
2012-12-14 02:38:55 +0700

    Change to Show each Question result

--- src/g53/exceedvote/ui/ResultUI.java
+++ src/g53/exceedvote/ui/ResultUI.java
@@ -1,14 +1,24 @@
 package g53.exceedvote.ui;
 
 
+import java.awt.BorderLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import sun.launcher.resources.launcher;
+import sun.org.mozilla.javascript.internal.annotations.JSConstructor;
+
+import g53.exceedvote.controller.Controller;
+import g53.exceedvote.domain.Question;
 import g53.exceedvote.persistence.DaoFactory;
 import g53.exceedvote.persistence.VoterDao;
 
@@ -23,26 +33,55 @@ import g53.exceedvote.persistence.VoterDao;
  */
 public class ResultUI {
 	private JFrame frame;
-	private VoterDao vote;
+	private Controller controller;
+	private ResourceBundle language;
+	private JComboBox<Question> questionslist;
+	private Question[] questions;
+	private JScrollPane scrollPane;
+	private String qname = "";
+	private int qid; 
 	
-	public ResultUI() {
+	public ResultUI(Controller control,ResourceBundle language) {
+		controller = control;
+    	this.language = language;
 		frame = new JFrame("Vote Result");
-		vote = DaoFactory.getInstance().getVoterDao();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		initComponents();
+		frame.setLayout(new BorderLayout());
+	    initComponents();
 		frame.pack();
 		frame.setSize(800, 500);
 		frame.setLocation(280, 100);
 	}
 	public void initComponents() {
-		try {
-			JOptionPane.showMessageDialog(frame,new JScrollPane(new JTable(vote.voteResult(null))),"Vote Result",JOptionPane.INFORMATION_MESSAGE);
-		} catch (HeadlessException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		questions = new Question[controller.getQuestion().size()];
+	    controller.getQuestion().toArray(questions);
+		questionslist = new JComboBox<Question>(questions);
+		scrollPane = new JScrollPane();
+		questionslist.addActionListener( new QuestionListener());
+		frame.add(questionslist,BorderLayout.NORTH);
+		frame.add(scrollPane,BorderLayout.CENTER);
 	}
 	public void run() {
 		frame.setVisible(true);
+	}
+	class QuestionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			qid = (int) (questionslist.getItemAt(questionslist.getSelectedIndex()).getQuestionID());
+			try {
+				scrollPane.add( new JTable(controller.voteResult(null, qid)));
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		LanguageUI languageUI = new LanguageUI();
+		ResourceBundle language = languageUI.getLanguage();
+		Controller con = new Controller();
+		con.connect();
+		ResultUI ui = new ResultUI(con, language);
+		ui.run();
 	}
 }
