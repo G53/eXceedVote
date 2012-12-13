b227a9aac09c2dd832069042d9d1546e9f98e2df
peepo3663 <peepo157@gmail.com>
2012-12-13 22:42:42 +0700

    Changing some part

--- src/g53/exceedvote/domain/Project.java
+++ src/g53/exceedvote/domain/Project.java
@@ -55,7 +55,7 @@ public class Project {
 	 * Display the string to shoe the description of project
 	 */
 	public String toString(){
-		return "Name: " + projectName + " Team name: " + teamName +	" Project ID: " + projectID;
+		return projectName;
 	}
 	
 	/**
